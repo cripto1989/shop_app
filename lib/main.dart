75879f78c006d096607ffae9bfa67ee4ad97bda0
Arturo Herrera <cripto1989@gmail.com>
2020-10-21 18:48:08 -0500

    Continuation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/constants.dart';
 import 'package:shop_app/routs.dart';
 import 'package:shop_app/screens/splash/splash_screen.dart';
+
+import 'theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,16 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.white,
-        fontFamily: "Muli",
-        textTheme: TextTheme(
-          bodyText1: TextStyle(color: kTextColor),
-          bodyText2: TextStyle(color: kTextColor),
-        ),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: theme(),
       // home: SplashScreen(),
       initialRoute: SplashScreen.routeName,
       routes: routes,
---
+++ lib/screens/sign_in/components/body.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/components/custom_surfix_icon.dart';
+import 'package:shop_app/size_config.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: getProportionateScreenWidth(8.0)),
+          child: Column(children: [
+            Text(
+              "Welcome Back",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: getProportionateScreenWidth(28),
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Sign in with your email and password \nor continue with social media",
+              textAlign: TextAlign.center,
+            ),
+            SignForm(),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class SignForm extends StatefulWidget {
+  @override
+  _SignFormState createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(  
+            keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  labelText: "Email",
+                  hintText: "Enter your email",
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  suffixIcon: CustomSurffixIcon(
+                    svgIcon: "assets/icons/Mail.svg",
+                  ))),
+        ],
+      ),
+    );
+  }
+}
